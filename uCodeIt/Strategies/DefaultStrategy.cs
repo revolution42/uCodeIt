cc0b594f41e6024a347b52004c92f7cdf1086aa7
benjamin.howarth <benjamin@codegecko.com>
2013-10-14 03:55:56 +0100

    Create strategy factory to enable overriding default strategy.

--- uCodeIt/Strategies/DefaultStrategy.cs
+++ uCodeIt/Strategies/DefaultStrategy.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Umbraco.Core.Services;
 
 namespace uCodeIt.Strategies
 {
     public class DefaultStrategy : IDoctypeInitStrategy
     {
+        public IContentTypeService ContentTypeService { get; set; }
+
         public void Process(IEnumerable<Type> types)
         {
-
+            foreach (var type in types)
+            {
+                foreach (var prop in type.GetProperties()) {
+                    // prop.Name
+                }
+            }
         }
 
     }
---
+++ uCodeIt/Strategies/DoctypeStrategyFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+
+namespace uCodeIt.Strategies
+{
+    public class DoctypeStrategyFactory {
+        
+        private static IDoctypeInitStrategy _strategy = null;
+        
+        public static DoctypeStrategyFactory Current {
+            get {
+                if (_strategy == null) _strategy = new DefaultStrategy();
+                return new DoctypeStrategyFactory(_strategy.GetType());
+            }
+        }
+
+        public DoctypeStrategyFactory(Type strategyType)
+        {
+            if (!strategyType.Inherits<IDoctypeInitStrategy>()) throw new ArgumentException();
+            _strategy = (IDoctypeInitStrategy)strategyType.GetConstructor(null).Invoke(null);
+
+        }
+
+        public static void SetStrategy(IDoctypeInitStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+    }
+}
