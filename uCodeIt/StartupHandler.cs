6576249d47acf7e9b4f3f2f2b320f5b7889f3f73
benjamin.howarth <benjamin@codegecko.com>
2013-10-14 03:31:45 +0100

    Added core Umbraco binaries and Nuget package restore. Framework for processing doctype gen strategies on initialization of Umbraco app.

---
+++ uCodeIt/StartupHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+
+namespace uCodeIt
+{
+    public class StartupHandler : IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+           
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            
+        }
+    }
+}
---
+++ uCodeIt/Strategies/IDoctypeInitStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uCodeIt.Strategies
+{
+    public interface IDoctypeInitStrategy
+    {
+        void Process(IEnumerable<Type> types);
+    }
+}
