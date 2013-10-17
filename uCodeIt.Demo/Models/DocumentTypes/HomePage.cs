a65ea69b2ae7d9587db1709c7bc58557c39c914b
Aaron Powell <me@aaron-powell.com>
2013-10-17 13:24:17 +1100

    setting up demo project

---
+++ uCodeIt.Demo/Models/DocumentTypes/HomePage.cs
@@ -0,0 +1,14 @@
+﻿using uCodeIt.DocumentTypes;
+
+namespace uCodeIt.Demo.Models.DocumentTypes
+{
+    [DocumentType]
+    public class HomePage : DocumentTypeBase
+    {
+        [Property]
+        public string Title { get; set; }
+
+        [Property]
+        public string BodyContent { get; set; }
+    }
+}
