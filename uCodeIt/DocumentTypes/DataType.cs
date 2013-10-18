3d3c371927d34aa3ba3172e36ce52ae397eac703
Aaron Powell <me@aaron-powell.com>
2013-10-18 15:45:38 +1100

    d'oh, meant to use consts not readonlys

--- uCodeIt/DocumentTypes/DataType.cs
+++ uCodeIt/DocumentTypes/DataType.cs
@@ -2,30 +2,30 @@
 {
     public static class DataType
     {
-        public static readonly string ApprovedColor = "Approved Color";
-        public static readonly string AuthorPicker = "Author Picker";
-        public static readonly string CheckboxList = "Checkbox List";
-        public static readonly string ContentPicker = "Content Picker";
-        public static readonly string DatePicker = "Date Picker";
-        public static readonly string DatePickerWithTime = "Date Picker with time";
-        public static readonly string Dropdown = "Dropdown";
-        public static readonly string DropdownMultiple = "Dropdown multiple";
-        public static readonly string FolderBrowser = "Folder Browser";
-        public static readonly string ImageCropper = "Image Cropper";
-        public static readonly string Label = "Label";
-        public static readonly string MacroContainer = "Macro Container";
-        public static readonly string MediaPicker = "Media Picker";
-        public static readonly string MemberPicker = "MemberPicker";
-        public static readonly string Numeric = "Numeric";
-        public static readonly string Radiobox = "Radiobox";
-        public static readonly string RelatedLinks = "Related Links";
-        public static readonly string RichTextEditor = "Richtext editor";
-        public static readonly string SimpleEditor = "SimpleEditor";
-        public static readonly string Tags = "Tags";
-        public static readonly string TextboxMultiple = "Textbox Multiple";
-        public static readonly string TextString = "Textstring";
-        public static readonly string TrueFalse = "True/false";
-        public static readonly string UltimatePicker = "Ultimate Picker";
-        public static readonly string Upload = "Upload";
+        public const string ApprovedColor = "Approved Color";
+        public const string AuthorPicker = "Author Picker";
+        public const string CheckboxList = "Checkbox List";
+        public const string ContentPicker = "Content Picker";
+        public const string DatePicker = "Date Picker";
+        public const string DatePickerWithTime = "Date Picker with time";
+        public const string Dropdown = "Dropdown";
+        public const string DropdownMultiple = "Dropdown multiple";
+        public const string FolderBrowser = "Folder Browser";
+        public const string ImageCropper = "Image Cropper";
+        public const string Label = "Label";
+        public const string MacroContainer = "Macro Container";
+        public const string MediaPicker = "Media Picker";
+        public const string MemberPicker = "MemberPicker";
+        public const string Numeric = "Numeric";
+        public const string Radiobox = "Radiobox";
+        public const string RelatedLinks = "Related Links";
+        public const string RichTextEditor = "Richtext editor";
+        public const string SimpleEditor = "SimpleEditor";
+        public const string Tags = "Tags";
+        public const string TextboxMultiple = "Textbox Multiple";
+        public const string TextString = "Textstring";
+        public const string TrueFalse = "True/false";
+        public const string UltimatePicker = "Ultimate Picker";
+        public const string Upload = "Upload";
     }
 }
