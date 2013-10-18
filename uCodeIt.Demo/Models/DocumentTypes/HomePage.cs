9de8a0b2a915c11fa67d97f17671591012de6718
Aaron Powell <me@aaron-powell.com>
2013-10-18 17:33:10 +1100

    property updating demo

--- uCodeIt.Demo/Models/DocumentTypes/HomePage.cs
+++ uCodeIt.Demo/Models/DocumentTypes/HomePage.cs
@@ -5,10 +5,16 @@ namespace uCodeIt.Demo.Models.DocumentTypes
     [DocumentType(Alias = "Home")]
     public class HomePage : DocumentTypeBase
     {
-        [Property(DataType = DataType.TextString)]
+        [Property(DataType = DataType.TextString, Tab = Tab.Tab2, Name = "Second Title")]
         public string Title { get; set; }
 
-        [Property(DataType = DataType.TextString)]
+        [Property(DataType = DataType.TextString, Tab = Tab.Content)]
         public string BodyContent { get; set; }
+
+        public enum Tab
+        {
+            Content = 1,
+            Tab2 = 2
+        }
     }
 }
