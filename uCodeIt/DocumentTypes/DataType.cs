26b205744bdd18137e601c1034b6e1c254d72655
Aaron Powell <me@aaron-powell.com>
2013-10-18 15:42:28 +1100

    Listing out default data types

---
+++ uCodeIt/DocumentTypes/DataType.cs
@@ -0,0 +1,31 @@
+﻿namespace uCodeIt.DocumentTypes
+{
+    public static class DataType
+    {
+        public static readonly string ApprovedColor = "Approved Color";
+        public static readonly string AuthorPicker = "Author Picker";
+        public static readonly string CheckboxList = "Checkbox List";
+        public static readonly string ContentPicker = "Content Picker";
+        public static readonly string DatePicker = "Date Picker";
+        public static readonly string DatePickerWithTime = "Date Picker with time";
+        public static readonly string Dropdown = "Dropdown";
+        public static readonly string DropdownMultiple = "Dropdown multiple";
+        public static readonly string FolderBrowser = "Folder Browser";
+        public static readonly string ImageCropper = "Image Cropper";
+        public static readonly string Label = "Label";
+        public static readonly string MacroContainer = "Macro Container";
+        public static readonly string MediaPicker = "Media Picker";
+        public static readonly string MemberPicker = "MemberPicker";
+        public static readonly string Numeric = "Numeric";
+        public static readonly string Radiobox = "Radiobox";
+        public static readonly string RelatedLinks = "Related Links";
+        public static readonly string RichTextEditor = "Richtext editor";
+        public static readonly string SimpleEditor = "SimpleEditor";
+        public static readonly string Tags = "Tags";
+        public static readonly string TextboxMultiple = "Textbox Multiple";
+        public static readonly string TextString = "Textstring";
+        public static readonly string TrueFalse = "True/false";
+        public static readonly string UltimatePicker = "Ultimate Picker";
+        public static readonly string Upload = "Upload";
+    }
+}
