d25592c479ce1e8ef3ae5c1db44e25684aaaeafc
Shannon Barratt <shannon@onlinemind.net>
2013-10-19 17:52:32 +0800

    Create build script

---
+++ BuildPackage/Build.cmd
@@ -0,0 +1,2 @@
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m /t:Package /p:"Configuration=Release";"MinUmbracoVersion=6.1.0";"PackageVersion=0.0.1-build";"BuildInParallel=true" Release.msbuild
+
